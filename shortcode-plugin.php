e056d8f545c7fbcbf147ac243ee093fe464ef443
Kishan Vyas <kishanvyas308@gmail.com>
2025-05-18 21:53:29 +0530

    first commit

---
+++ shortcode-plugin.php
@@ -0,0 +1,11 @@
+<?php
+
+
+/**
+ * Plugin Name: Shortcode Plugin
+ * Description: A simple WordPress plugin to demonstrate shortcode functionality.
+ * Version: 1.0
+ * Author: Kishan Vyas
+ * Author URI: https://kishanvyas.tech
+ * 
+ */
